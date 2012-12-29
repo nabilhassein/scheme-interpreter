458fe9372cfa0705f7a7fb4698aa45f30d5ee2a6
Nabil Hassein <nabil.hassein@gmail.com>
2012-12-29 12:20:53 -0500

    modified evaluator to work with environments

--- eval.hs
+++ eval.hs
@@ -10,45 +10,45 @@ import Control.Monad.Error (throwError)
 import System.Environment (getArgs, getProgName)
 import System.IO
 
-eval :: LispVal -> ThrowsError LispVal
-eval val@(Number _)             = return val
-eval val@(Complex _)            = return val
-eval val@(Real _)               = return val
-eval val@(Ratio _)              = return val
-eval val@(String _)             = return val
-eval val@(Character _)          = return val
-eval val@(Boolean _)            = return val
-eval (List [Atom "quote", val]) = return val
-eval (List [Atom "if", predicate, consequent, alternative]) = do
-  result <- eval predicate
-  eval $ case result of
+eval :: Env -> LispVal -> ThrowsError LispVal
+eval env val@(Number _)             = return val
+eval env val@(Complex _)            = return val
+eval env val@(Real _)               = return val
+eval env val@(Ratio _)              = return val
+eval env val@(String _)             = return val
+eval env val@(Character _)          = return val
+eval env val@(Boolean _)            = return val
+eval env (List [Atom "quote", val]) = return val
+eval env (List [Atom "if", predicate, consequent, alternative]) = do
+  result <- eval env predicate
+  eval env $ case result of
     Boolean False -> alternative
     _             -> consequent
 -- TODO: how to retain original form in error message after recursing in cond and case?
-eval form@(List (Atom "cond" : clauses)) =
+eval env form@(List (Atom "cond" : clauses)) =
   if null clauses
   then throwError $ BadSpecialForm "no true clause in cond expression: " form
   else case head clauses of
-    List [Atom "else", expr] -> eval expr
-    List [test, expr]        -> eval $ List [Atom "if",
+    List [Atom "else", expr] -> eval env expr
+    List [test, expr]        -> eval env $ List [Atom "if",
                                              test,
                                              expr,
                                              List (Atom "cond" : tail clauses)]
     _ -> throwError $ BadSpecialForm "ill-formed cond expression: " form
-eval form@(List (Atom "case" : key : clauses)) =
+eval env form@(List (Atom "case" : key : clauses)) =
   if null clauses
   then throwError $ BadSpecialForm "no true clause in case expression: " form
   else case head clauses of
-    List (Atom "else" : exprs)   -> mapM eval exprs >>= return . last
+    List (Atom "else" : exprs)   -> mapM (eval env) exprs >>= return . last
     List ((List datums) : exprs) -> do
-      result <- eval key
+      result <- eval env key
       equality <- mapM (\x -> eqv [result, x]) datums
       if Boolean True `elem` equality
-        then mapM eval exprs >>= return . last
-        else eval $ List (Atom "case" : key : tail clauses)
+        then mapM (eval env) exprs >>= return . last
+        else eval env $ List (Atom "case" : key : tail clauses)
     _ -> throwError $ BadSpecialForm "ill-formed case expression: " form
-eval (List (Atom f : args))     = mapM eval args >>= apply f
-eval badForm                    = throwError $ BadSpecialForm
+eval env (List (Atom f : args))     = mapM (eval env) args >>= apply f
+eval env badForm                    = throwError $ BadSpecialForm
                                   "unrecognized special form" badForm
 
 apply :: String -> [LispVal] -> ThrowsError LispVal
@@ -60,9 +60,9 @@ apply f args = maybe (throwError $ NotFunction "unrecognized primitive function 
 readPrompt :: String -> IO String
 readPrompt prompt = putStr prompt >> hFlush stdout >> getLine
 
-evalAndPrint :: String -> IO ()
-evalAndPrint expr = putStrLn =<< (return . extractValue . trapError .
-                                  fmap show $ readExpr expr >>= eval)
+evalAndPrint :: Env -> String -> IO ()
+evalAndPrint env expr = putStrLn =<< (return . extractValue . trapError .
+                                  fmap show $ readExpr expr >>= eval env)
 
 untilM_ :: Monad m => (a -> Bool) -> m a -> (a -> m ()) -> m ()
 untilM_ pred prompt action = do
@@ -71,8 +71,11 @@ untilM_ pred prompt action = do
     then return ()
     else action result >> untilM_ pred prompt action
 
+runExpr :: String -> IO ()
+runExpr expr = nullEnv >>= flip evalAndPrint expr
+
 repl :: IO ()
-repl = untilM_ (`elem` ["quit", "exit"]) (readPrompt "scheme> ") evalAndPrint
+repl = nullEnv >>= untilM_ (`elem` ["quit", "exit"]) (readPrompt "scheme> ") . evalAndPrint
 
 
 readExpr :: String -> ThrowsError LispVal
@@ -85,7 +88,7 @@ main = do
   args <- getArgs
   case length args of
     0 -> repl
-    1 -> evalAndPrint $ head args
+    1 -> runExpr $ head args
     _ -> do
       progName <- getProgName
       putStrLn $ "Usage: " ++ progName ++ " [expr]"
