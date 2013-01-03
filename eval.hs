2f2ac9337aca04af340ac5c2c447a4a31ddcfd0b
Nabil Hassein <nabil.hassein@gmail.com>
2013-01-03 09:54:33 -0500

    added variables and assignment

--- eval.hs
+++ eval.hs
@@ -10,7 +10,7 @@ import Control.Monad.Error (throwError)
 import System.Environment (getArgs, getProgName)
 import System.IO
 
-eval :: Env -> LispVal -> ThrowsError LispVal
+eval :: Env -> LispVal -> IOThrowsError LispVal
 eval env val@(Number _)             = return val
 eval env val@(Complex _)            = return val
 eval env val@(Real _)               = return val
@@ -18,6 +18,7 @@ eval env val@(Ratio _)              = return val
 eval env val@(String _)             = return val
 eval env val@(Character _)          = return val
 eval env val@(Boolean _)            = return val
+eval env (Atom id)                  = getEnv env id
 eval env (List [Atom "quote", val]) = return val
 eval env (List [Atom "if", predicate, consequent, alternative]) = do
   result <- eval env predicate
@@ -39,17 +40,19 @@ eval env form@(List (Atom "case" : key : clauses)) =
   if null clauses
   then throwError $ BadSpecialForm "no true clause in case expression: " form
   else case head clauses of
-    List (Atom "else" : exprs)   -> mapM (eval env) exprs >>= return . last
-    List ((List datums) : exprs) -> do
+    List (Atom "else" : exprs) -> mapM (eval env) exprs >>= return . last
+    List (List datums : exprs) -> do
       result <- eval env key
-      equality <- mapM (\x -> eqv [result, x]) datums
+      equality <- mapM (\x -> liftThrows $ eqv [result, x]) datums
       if Boolean True `elem` equality
         then mapM (eval env) exprs >>= return . last
         else eval env $ List (Atom "case" : key : tail clauses)
     _ -> throwError $ BadSpecialForm "ill-formed case expression: " form
-eval env (List (Atom f : args))     = mapM (eval env) args >>= apply f
+eval env (List [Atom "set!", Atom var, form]) = eval env form >>= setVar env var
+eval env (List [Atom "define", Atom var, form]) = eval env form >>= defineVar env var
+eval env (List (Atom f : args))     = mapM (eval env) args >>= liftThrows . apply f
 eval env badForm                    = throwError $ BadSpecialForm
-                                  "unrecognized special form" badForm
+                                      "unrecognized special form" badForm
 
 apply :: String -> [LispVal] -> ThrowsError LispVal
 apply f args = maybe (throwError $ NotFunction "unrecognized primitive function args" f)
@@ -60,9 +63,11 @@ apply f args = maybe (throwError $ NotFunction "unrecognized primitive function 
 readPrompt :: String -> IO String
 readPrompt prompt = putStr prompt >> hFlush stdout >> getLine
 
+evalString :: Env -> String -> IO String
+evalString env expr = runIOThrows . fmap show $ (liftThrows $ readExpr expr) >>= eval env
+
 evalAndPrint :: Env -> String -> IO ()
-evalAndPrint env expr = putStrLn =<< (return . extractValue . trapError .
-                                  fmap show $ readExpr expr >>= eval env)
+evalAndPrint env expr = evalString env expr >>= putStrLn
 
 untilM_ :: Monad m => (a -> Bool) -> m a -> (a -> m ()) -> m ()
 untilM_ pred prompt action = do
